6343140178c0d8b8ca229696a6f2132d3bdcc520
Tharaka Lakmal <tharakalakmal60@gmail.com>
2024-09-15 02:01:28 +0530

    Create backend

---
+++ gem-prediction-backend/src/main/java/com/gemProphecy/gem_prediction_backend/GemPredictionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gemProphecy.gem_prediction_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemPredictionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemPredictionBackendApplication.class, args);
+	}
+
+}
---
+++ gem-prediction-backend/src/test/java/com/gemProphecy/gem_prediction_backend/GemPredictionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemProphecy.gem_prediction_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemPredictionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
